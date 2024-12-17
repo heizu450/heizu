6203aa27300a3e82160fd55d04d5030e528df5e2
heizu <heizu450@gmail.com>
2024-12-17 20:50:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let toggle = document.querySelector('.toggle');
+    let body = document.querySelector('body');
+    toggle.addEventListener('click', function() {
+        body.classList.toggle('open');
+    });
+});
